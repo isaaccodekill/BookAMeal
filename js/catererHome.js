f27699a2eeeec154eac4c74e93bdd96b6f8320c0
isaaccodekill <isaacbello3200@gmail.com>
2019-02-10 07:16:17 -1200

    create the caterer/admin related pages

---
+++ js/catererHome.js
@@ -0,0 +1,105 @@
+const closed = document.querySelector(".close")
+const Overlay = document.querySelector(".show-card")
+const innerShrink = document.querySelector(".card-expanded")
+const card = document.querySelectorAll(".card")
+const cardlist = document.querySelector(".card-list")
+const navItem = document.querySelectorAll(".nav-item")
+const mealOptionPage = document.querySelector(".Meal-options")
+const menuPage = document.querySelector(".Menu")
+const orderPage = document.querySelector(".order-history")
+const menuList = document.querySelector(".card-list-menu")
+const noItems = document.querySelector(".no-items")
+const body = document.querySelector("body")
+
+
+const mealOptions = document.querySelector(".a")
+const todayMenu = document.querySelector(".b")
+const orderHistory = document.querySelector(".c")
+
+cardlist.addEventListener("click", function (e) {
+    if (e.target.matches(".text-cover")) {
+        let cardElement = e.target.parentElement
+        Overlay.style.display = "block"
+        innerShrink.classList.add("shrink")
+        Overlay.style.opacity = "1"
+        setTimeout(() => {
+            innerShrink.classList.remove("shrink")
+        }, 500)
+    }
+})
+
+
+closed.addEventListener("click", () => {
+    Overlay.style.opacity = "0";
+    innerShrink.classList.add("shrink")
+    setTimeout(() => {
+        Overlay.style.display = "none"
+    }, 500)
+})
+
+
+// slide in the nav bar
+
+const menu = document.querySelector(".hamburger")
+const overlay = document.querySelector(".overlay")
+const sidebar = document.querySelector(".sidebar")
+const closed2 = document.querySelector(".closed")
+
+menu.addEventListener("click", () => {
+    overlay.style.display = "block"
+    sidebar.style.display = "block"
+    sidebar.classList.add("animated")
+    sidebar.classList.add("slideInLeft")
+})
+
+closed2.addEventListener("click", () => {
+    sidebar.classList.remove("slideInLeft")
+    sidebar.classList.add("slideOutLeft")
+    let me = setTimeout(() => {
+        sidebar.style.display = "none"
+        sidebar.classList.remove("slideOutLeft")
+        overlay.style.display = "none"
+        clearTimeout(me)
+    }, 700)
+})
+
+
+
+navItem.forEach(nav => {
+    nav.addEventListener("click", function () {
+        sidebar.classList.remove("slideInLeft")
+        sidebar.classList.add("slideOutLeft")
+        let me = setTimeout(() => {
+            sidebar.style.display = "none"
+            sidebar.classList.remove("slideOutLeft")
+            overlay.style.display = "none"
+            clearTimeout(me)
+        }, 700)
+    })
+})
+
+mealOptions.addEventListener("click", function () {
+    mealOptionPage.style.display = "block"
+    menuPage.style.display = "none"
+    orderPage.style.display = "none" 
+})
+
+
+todayMenu.addEventListener("click", function () {
+    mealOptionPage.style.display = "none"
+    menuPage.style.display = "block"
+    orderPage.style.display = "none" 
+})
+
+
+
+orderHistory.addEventListener("click", function () {
+    mealOptionPage.style.display = "none"
+    menuPage.style.display = "none"
+    orderPage.style.display = "block" 
+})
+
+
+if (menuList.innerHTML === "") {
+    noItems.style.display = "block"
+}
