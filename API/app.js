4919c7ca0b111ae327acc36c1f50fde5d454d16a
isaaccodekill <isaacbello3200@gmail.com>
2019-02-13 02:59:20 +0100

    Set up babel transpiling enironment

--- API/app.js
+++ API/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(app.get('port'));
+const PORT = 8080;
+app.listen(PORT);
