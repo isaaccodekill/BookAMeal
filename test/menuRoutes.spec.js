c333359e52dea69ceecb21a981bd10d8fc66a8d4
isaaccodekill <isaacbello3200@gmail.com>
2019-03-07 15:30:42 +0100

    Edit test files again

--- test/menuRoutes.spec.js
+++ test/menuRoutes.spec.js
@@ -16,36 +16,36 @@ dotenv.config();
 let CatererIdAccessible;
 let UserIdAccessible;
 
-before((done) => {
-  Caterer.create({
-    name: 'zikosis',
-    email: 'newemail2@gmail.com',
-    password: 'hashedPassword', // dont try this at home
-    phoneNumber: '12345678901',
-    restaurant: 'binat foods',
-  })
-    .then((caterer) => {
-      CatererIdAccessible = caterer.id;
-      User.create({
-        name: 'zikUser',
-        email: 'newuser@gmail.com',
-        password: 'hashedPassword', // dont try this at home
-        phoneNumber: '12345678901',
-      })
-        .then((user) => {
-          UserIdAccessible = user.id;
-          done();
-        });
-    });
-});
+// before((done) => {
+//   Caterer.create({
+//     name: 'zikosis',
+//     email: 'newemail2@gmail.com',
+//     password: 'hashedPassword', // dont try this at home
+//     phoneNumber: '12345678901',
+//     restaurant: 'binat foods',
+//   })
+//     .then((caterer) => {
+//       CatererIdAccessible = caterer.id;
+//       User.create({
+//         name: 'zikUser',
+//         email: 'newuser@gmail.com',
+//         password: 'hashedPassword', // dont try this at home
+//         phoneNumber: '12345678901',
+//       })
+//         .then((user) => {
+//           UserIdAccessible = user.id;
+//           done();
+//         });
+//     });
+// });
 
-after((done) => {
-  Caterer.destroy({ where: { email: 'newemail2@gmail.com' } })
-    .then((res) => {
-      User.destroy({ where: { email: 'newuser@gmail.com' } })
-      done();
-    });
-});
+// after((done) => {
+//   Caterer.destroy({ where: { email: 'newemail2@gmail.com' } })
+//     .then((res) => {
+//       User.destroy({ where: { email: 'newuser@gmail.com' } })
+//       done();
+//     });
+// });
 
 /* eslint-disable no-unused-vars */
 const should = chai.should();
@@ -68,7 +68,7 @@ describe('GET /api/v1/menu', () => {
         res.body.should.be.a('object');
         res.body.status.should.equal('successful');
         res.body.menu.should.be.a('object');
-        // res.body.menu.should.be.a('array');
+        res.body.menu.should.be.a('array');
         done();
       });
   });
